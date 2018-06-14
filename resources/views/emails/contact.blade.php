e8fb242f4f60954f176e0973d1e2b8bbb832070d
vagrant <vagrant@homestead>
2018-06-14 21:31:56 +0000

    changed mailer font

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')
-# Introduction
+# New Contact Submission From Website
 
-#####Hi Yael
-######You have a new contact form submission from your website
-######From: {{$contact->contactName}}
-$$$$$$Email: <a href="mailto:{{$contact->email}}">{{$contact->email}}</a>
+##Hi Yael
+###You have a new contact form submission from your website
+####From: {{$contact->contactName}}
+####Email: <a href="mailto:{{$contact->email}}">{{$contact->email}}</a>
 @component('mail::panel')
     Message: {{$contact->comments}}
 @endcomponent
